0ec867f6d4ff337e20c4094dcd3c24c70a943b62
HUCORP <53086460+HUCORP-admin@users.noreply.github.com>
2024-01-01 21:00:09 -0500

    Added Final Code

---
+++ Final Code/assets/tiled/images.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="images" tilewidth="64" tileheight="32" tilecount="2" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="32" height="32" source="tiles/blue.png"/>
+ </tile>
+ <tile id="2">
+  <image width="64" height="22" source="tiles/bridge.png"/>
+ </tile>
+</tileset>
