86ced4b1bcae1cbc42a226fd64c5a89b468df6a6
HUCORP <53086460+HUCORP-admin@users.noreply.github.com>
2024-01-01 20:52:55 -0500

    Delete desktop directory

--- desktop/build/resources/main/tiled/objects.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="objects" tilewidth="32" tileheight="32" tilecount="7" columns="7">
- <image source="object-tileset.png" width="224" height="32"/>
-</tileset>
